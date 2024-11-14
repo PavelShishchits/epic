1302f0c86e6762f8f9635a12e6e7d9fd5dbf94f0
pavel s <p.shischits@ext.kuehlhaus.com>
2024-11-14 15:43:39 +0100

    add sign up route

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign up",
+  description: "Sign up",
+}
+
+export default function SignUpPage() {
+  return (<div>
+    <h1>Sign up</h1>
+    <form>
+    </form>
+  </div>)
+}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="p-6 border-2 border-sky-100">
-      <Link href="/">Logo</Link>
+      <div className="flex justify-between gap-3">
+        <Link href="/">Logo</Link>
+        <Link href="/sign-up">Sign up</Link>
+      </div>
     </header>
   );
 };
