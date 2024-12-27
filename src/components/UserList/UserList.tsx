a223eee1578ade7ea8b4d6983640ce6c359ed5dd
pavel s <p.shischits@ext.kuehlhaus.com>
2024-12-27 14:19:08 +0100

    adapt raw query for postgresql

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -14,16 +14,15 @@ const UserList = async ({ usernameQuery }: UserListProps) => {
 
   const users = await prisma.$queryRaw<UserSearchResults>(
     Prisma.sql`
-      SELECT User.id, User.name, User.username, UserImage.id AS imageId
-      FROM User
-      LEFT JOIN UserImage ON UserImage.userId = User.id
-      WHERE User.username
-      LIKE ${like} OR User.name LIKE ${like}
+      SELECT "User"."id", "User"."name", "User"."username", "UserImage"."id" AS "imageId"
+      FROM "User"
+      LEFT JOIN "UserImage" ON "UserImage"."userId" = "User"."id"
+      WHERE "User"."username" ILIKE ${like} OR "User"."name" ILIKE ${like}
       ORDER BY (
-        SELECT Note.updatedAt
-        FROM Note
-        WHERE Note.ownerId = User.id
-        ORDER BY Note.updatedAt DESC
+        SELECT "Note"."updatedAt"
+        FROM "Note"
+        WHERE "Note"."ownerId" = "User"."id"
+        ORDER BY "Note"."updatedAt" DESC
         LIMIT 1
       ) DESC
       LIMIT 50
